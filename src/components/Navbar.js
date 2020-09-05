300ba966b2efa89ae4a1242869fa1b36641d07c9
Miguel Angel <miguelapb35@gmail.com>
2020-09-04 21:55:36 -0500

    navbar and products added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,25 +2,27 @@ import React from 'react'
 import {Link} from 'gatsby'
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to='/' >Home</Link>
-        </li>
-        <li>
-          <Link to='/blog' >Blog</Link>
-        </li>
-        <li>
-          <Link to='/products' >Products</Link>
-        </li>
-        <li>
-          <Link to='/examples' >Examples</Link>
-        </li>
-        <li>
-          <Link to='/images' >Images</Link>
-        </li>
-      </ul>
-    </nav>
+    <header className='header'>
+      <nav>
+        <ul className='navList'>
+          <li>
+            <Link to='/' className='navItem' >Home</Link>
+          </li>
+          <li>
+            <Link to='/blog' className='navItem'>Blog</Link>
+          </li>
+          <li>
+            <Link to='/products' className='navItem' >Products</Link>
+          </li>
+          <li>
+            <Link to='/examples' className='navItem'>Examples</Link>
+          </li>
+          <li>
+            <Link to='/images' className='navItem'>Images</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
   )
 }
 
